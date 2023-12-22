3e4e8684492cf22d9a66cc5125ee3409d5d67335
hazecodez <muhammedhashir298@gmail.com>
2023-12-22 23:53:26 +0530

    Style added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import './App.css'
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="container">
+        <div className="to-do">
+            <h2>To-Do-List</h2>
+            <div className="row">
+                <input type="text" id="inputBox" placeholder="Add your text"/>
+                <button>Add</button>
+            </div>
+            <ul id="list-container"></ul>
+        </div>
+
+    </div>
   );
 }
 
